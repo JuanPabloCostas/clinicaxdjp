2279b8290813a6f8a4a3c3f15ee81f3f01347bc1
Juan Antonio HR <92959694+JuanAntonioHR@users.noreply.github.com>
2024-11-13 12:46:54 -0600

    Refactor global styles, update Tailwind configuration, and add utility functions

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,14 +2,14 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
+const lexendZetta = localFont({
+  src: "./fonts/LexendZetta-VariableFont_wght.woff",
+  variable: "--font-lexend-zetta",
   weight: "100 900",
 });
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
+const jakartaSans = localFont({
+  src: "./fonts/PlusJakartaSans-VariableFont_wght.woff",
+  variable: "--font-jakarta-sans",
   weight: "100 900",
 });
 
@@ -26,7 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${lexendZetta.variable} ${jakartaSans.variable} antialiased`}
       >
         {children}
       </body>
