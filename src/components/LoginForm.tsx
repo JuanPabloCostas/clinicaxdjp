6d54d50f4dcf5a2dbf4e33c0e5c81dcb503343fb
Juan Pablo Costas Rueda <jcostas29@alumnos.uaq.mx>
2024-11-13 12:54:05 -0600

    LoginPage

---
+++ src/components/LoginForm.tsx
@@ -0,0 +1,10 @@
+export default function LoginForm() {
+    
+    return (
+        <>
+            <div className="flex flex-col items-start gap-8">
+                <p></p>
+            </div>
+        </>
+    )
+}
