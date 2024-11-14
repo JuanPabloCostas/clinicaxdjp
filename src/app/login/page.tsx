caf3783c771e49d8c808435bda9acce043daad43
Juan Pablo Costas Rueda <jcostas29@alumnos.uaq.mx>
2024-11-13 19:07:17 -0600

    Sidebar and header

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,14 +3,16 @@ import LoginForm from "@/components/LoginForm";
 export default function page() {
 
     return (
-        <div className="w-1/2 min-w-fit mx-auto">
-            <div className="w-full inline-flex flex-col gap-7 items-center">
-                <div className="flex flex-col gap-20 items-center">
-                    <h2 className="text-9xl">MEDIAMIN</h2>
-                    <h3 className="text-4xl">Inicia Sesion</h3>
-                </div>
-                <div className="w-full">
-                    <LoginForm />
+        <div className="flex justify-center items-center h-screen">
+            <div className="w-1/3 min-w-fit mx-auto">
+                <div className="w-full inline-flex flex-col gap-7 items-center">
+                    <div className="flex flex-col gap-10 items-center">
+                        <h2 className="text-9xl">MEDIAMIN</h2>
+                        <h3 className="text-4xl">Inicia Sesion</h3>
+                    </div>
+                    <div className="w-full">
+                        <LoginForm />
+                    </div>
                 </div>
             </div>
         </div>
---
+++ src/components/Logo.tsx
@@ -0,0 +1,7 @@
+export default function Logo() {
+    return (
+        <div>
+            <h1>MEDIADMIN</h1>
+        </div>
+    )
+}
