26e1f6a767b1daab088b919be9cc64a06bd4b336
Juan Antonio HR <92959694+JuanAntonioHR@users.noreply.github.com>
2024-11-15 14:41:19 -0600

    feat: adjust layout and header height for better responsiveness

--- src/app/(application)/layout.tsx
+++ src/app/(application)/layout.tsx
@@ -7,12 +7,12 @@ export default function DashboardLayout({
     children: React.ReactNode
   }) {
     return (
-      <section className="px-16">
+      <section className="px-16 h-screen">
         {/* Include shared UI here e.g. a header or sidebar */}
         <Header />
-        <div className="grid grid-cols-5 gap-12">
+        <div className="grid grid-cols-5 gap-12 pb-10 h-[90vh]">
           <SideBar />
-          <div className="w-full px-4 col-span-4">
+          <div className="w-full h-full px-4 col-span-4 overflow-y-auto overflow-hidden">
             {children}
           </div>
         </div>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,7 +6,7 @@ import Logo from "./Logo";
 
 export default function Header() {
     return (
-        <div className="flex justify-between items-center self-stretch py-4">
+        <div className="flex justify-between items-center self-stretch py-4 h-[10vh]">
             <Logo/>
             <div className="flex justify-end items-center gap-6 w-2/5">
                 <MdSearch size={28} />
